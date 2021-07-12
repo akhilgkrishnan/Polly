92dc8ce993b136eedd448de1f2138603d920c4e8
akhilgkrishnan <akhilgkrishnan4u@gmail.com>
2021-07-12 22:59:52 +0530

    Added API based architecture

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
+import { initializeLogger } from "common/logger";
+import { setAuthHeaders } from "apis/axios";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
@@ -12,7 +14,7 @@ const App = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" render={() => <div>Home</div>} />
+        <Route exact path="/" render={() => <div>Home Page</div>} />
         <Route exact path="/about" render={() => <div>About</div>} />
       </Switch>
     </Router>
